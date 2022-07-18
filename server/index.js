e01cf104d1be137378be188897ea61fa20844071
Pravin Desai <pravindesai0790@gmail.com>
2022-07-18 17:03:59 +0530

    Created controller and routes JS files

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
+import videoRoutes from "./routes/videos.js"
+import commentRoutes from "./routes/comments.js"
 
 const app = express()
 dotenv.config()
@@ -12,6 +15,10 @@ const connect = () => {
         throw err;
     });
 }
+
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
 
 app.listen(8000, () => {
     connect();
